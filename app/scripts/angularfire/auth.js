6e07df78430ebcb8d71f20cc1382b52c87c19b2b
jessonfoo <jessonfoo@gmail.com>
2016-09-06 12:49:56 -0700

    formatting

--- app/scripts/angularfire/auth.js
+++ app/scripts/angularfire/auth.js
@@ -1,14 +1,7 @@
 'use strict';
-
 angular.module('firebase.Auth', [])
-
-  
-
-    .constant('SIMPLE_LOGIN_PROVIDERS', ['password','google'])
-
+  .constant('SIMPLE_LOGIN_PROVIDERS', ['password','google'])
   .constant('loginRedirectPath', '/login')
-
-
   .factory('auth', ["$firebaseAuth", function ($firebaseAuth) {
       return $firebaseAuth();
-    }]);
+  }]);
