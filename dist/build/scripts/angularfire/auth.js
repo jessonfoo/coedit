88eb87f23332fc954dd7c32c5e09c0bc281e0d75
jessonfoo <jessonfoo@gmail.com>
2016-09-15 21:11:48 -0700

    remove prebuilds

--- dist/build/scripts/angularfire/auth.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-angular.module('firebase.Auth', [])
-  .constant('SIMPLE_LOGIN_PROVIDERS', ['password','google'])
-  .constant('loginRedirectPath', '/login')
-  .factory('auth', ["$firebaseAuth", function ($firebaseAuth) {
-      return $firebaseAuth();
-  }]);
