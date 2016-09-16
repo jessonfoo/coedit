7bbeef7003f88774fa1dea76fbdf962a7fc72afb
jessonfoo <jessonfoo@gmail.com>
2016-09-15 17:35:27 -0700

    rewind

---
+++ build/scripts/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name coeditApp
+ * @description
+ * # coeditApp
+ *
+ * Main module of the application.
+ */
+angular.module('coeditApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase',
+    'firebase.Auth'
+  ]);
